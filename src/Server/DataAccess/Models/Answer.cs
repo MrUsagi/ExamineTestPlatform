e3d134d6b72c0dc565d741987ae7effda821befd
MrUsagi <anavisikim1@gmail.com>
2020-10-08 14:10:52 +0300

    Add Models
    Create basic models.

---
+++ src/Server/DataAccess/Models/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class Answer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public string Text { get; set; }
+    }
+}
