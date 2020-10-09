519ec25e507170fbf055974b2903daddc235ce2b
MrUsagi <anavisikim1@gmail.com>
2020-10-09 13:52:23 +0300

    Add Repositories & UOW

---
+++ src/DataAccess/Repository/QuestionRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repository
+{
+    public class QuestionRepository:BaseRepository<Question>
+    {
+        public QuestionRepository(TestsContext context) : base(context) { }
+    }
+}
---
+++ src/DataAccess/Repository/UserTestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repository
+{
+    public class UserTestRepository
+    {
+        public 
+    }
+}
