f2dd549c1370a3a43dcfd5b13fed42dcff09ab4b
Nick Bachurin <nick.dp.ua@gmail.com>
2020-10-10 01:00:54 +0300

    Finish UI logic
    Finish UI logic in ChangesTestPage/ Update UI

--- src/Client/TestPlatformProject/Pages/ChangesTestPage.xaml.cs
+++ src/Client/TestPlatformProject/Pages/ChangesTestPage.xaml.cs
@@ -26,20 +26,30 @@ namespace TestPlatformProject.Pages
 
             _service1Client = service1Client;
             AddAnswerButton.Click += clickLastButton;
+            AddQuestionButton.Click += clickFirstButton;
+        }
+
+        private void clickFirstButton(object sender, RoutedEventArgs e)
+        {
+            ChangesAnswerScrollPanel.Children.Add(new TextBlock() { FontSize = 15, FontStyle = FontStyles.Italic, Foreground = new SolidColorBrush(Color.FromRgb(255, 69, 0)), Margin = new Thickness(40, 30, 40, 0), Text = "Введите вопрос:" });
+            ChangesAnswerScrollPanel.Children.Add(new TextBox() {  Margin = new Thickness(40, 10, 40, 0) });
+            ChangesAnswerScrollPanel.Children.Add(new TextBlock() { FontSize = 15, FontStyle = FontStyles.Italic, Foreground = new SolidColorBrush(Color.FromRgb(255, 69, 0)), Margin = new Thickness(40, 30, 40, 0), Text = "Введите URL фото если оно надо:" });
+            ChangesAnswerScrollPanel.Children.Add(new TextBox() {  Margin = new Thickness(40, 10, 40, 0) });
+            ChangesAnswerScrollPanel.Children.Add(new StackPanel() { Orientation = Orientation.Horizontal });
+            ((StackPanel)ChangesAnswerScrollPanel.Children[ChangesAnswerScrollPanel.Children.Count - 1]).Children.Add(new TextBox() { Width = 200, Height = 17, Margin = new Thickness(40, 10, 20, 0) });
+            ((StackPanel)ChangesAnswerScrollPanel.Children[ChangesAnswerScrollPanel.Children.Count - 1]).Children.Add(new CheckBox() { Margin = new Thickness(15, 12, 0, 0) });
+
         }
 
         private void clickLastButton(object sender, RoutedEventArgs e)
         {
-
-           // AddAnswerButton.Click -= clickLastButton;
 
             ChangesAnswerScrollPanel.Children.Add(new TextBlock() { FontSize = 15, FontStyle = FontStyles.Italic, Foreground = new SolidColorBrush(Color.FromRgb(255, 69, 0)), Margin = new Thickness(40, 30, 40, 0), Text = "Вариант ответа:" });
 
             ChangesAnswerScrollPanel.Children.Add(new StackPanel() { Orientation = Orientation.Horizontal });
             ((StackPanel)ChangesAnswerScrollPanel.Children[ChangesAnswerScrollPanel.Children.Count - 1]).Children.Add(new TextBox() { Width = 200, Height = 17, Margin = new Thickness(40, 10, 20, 0) });
             ((StackPanel)ChangesAnswerScrollPanel.Children[ChangesAnswerScrollPanel.Children.Count - 1]).Children.Add(new CheckBox() { Margin = new Thickness(15, 12, 0, 0) });
-            // ChangesAnswerScrollPanel.Children.Add(new Button() { Width = 200, Margin = new Thickness(0, 15, 0, 0), Content = "_Добавить ответ", Background = new SolidColorBrush(Color.FromRgb(255, 69, 0)), Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255)), });
-            // AddAnswerButton.Click += clickLastButton;
+
         }
     }
 }
