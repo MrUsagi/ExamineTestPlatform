1d721874e7f00d7a748660a3f1e3ce6309de6483
Kuzmin00 <43570410+Kuzmin00@users.noreply.github.com>
2020-10-09 16:42:34 +0300

    Update Service
    Update Service

--- src/Server/TestPlatformServices/Service1.svc.cs
+++ src/Server/TestPlatformServices/Service1.svc.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccess.UOW;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TestPlatformServices
 {
@@ -15,53 +17,24 @@ namespace TestPlatformServices
     // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
     public class Service1 : IService1
     {
-        private IServiceProvider ServiceProvider;
-        private IConfiguration Configuration;
-        public bool Connect()
-        {
-
-
-
-
-
-            return true;
-        }
-
+        UnitOfWork unitOfWork = new UnitOfWork();
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
         }
 
-        public bool IsLogin(string password, string login)
+        public async Task<bool> IsLogin(string password, string login)
         {
+            if( (await unitOfWork.Logins.FindByConditionAsync(x=>x.Login == login)).Any()) return false;
+            
+            await unitOfWork.Logins.CreateAsync(new DataAccess.Models.LoginUser() { Password = password, Login = login});
+
             return true;
         }
 
         public bool Registration(string password, string login, string fio, string email)
         {
             throw new NotImplementedException();
-        }
-
-
-        private void ConfigurationService(IServiceCollection services)
-        {
-            ConfigurationManagerBll.Configuration(services, Configuration.GetConnectionString("SqlConnection"));
-
-           // services.AddSingleton(typeof(TCPServer));
-
-        }
-
-        private void OnStartup()
-        {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true);
-            Configuration = builder.Build();
-            var serviceCollection = new ServiceCollection();
-            
-            ConfigurationService(serviceCollection);
-            ServiceProvider = serviceCollection.BuildServiceProvider();
-
-            server = ServiceProvider.GetRequiredService<TCPServer>();
         }
     }
 }
