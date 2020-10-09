17dc748d94ba59284b9f21f3503315dcf9b12d2b
Pacha12 <shagpacha@gmail.com>
2020-10-09 17:11:04 +0300

    Update PageTakeTest
    Update PageTakeTest

--- src/Client/TestPlatformProject/Pages/PageTakeTest.xaml.cs
+++ src/Client/TestPlatformProject/Pages/PageTakeTest.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -18,6 +21,10 @@ namespace TestPlatformProject.Pages
     /// </summary>
     public partial class PageTakeTest : Page
     {
+        Test test = new Test();
+        
+        
+        
         public PageTakeTest()
         {
             InitializeComponent();
@@ -25,7 +32,61 @@ namespace TestPlatformProject.Pages
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
+
+            //////// тестировал  test переопределил в а и б но в foreach используется test
+            var a = test.Questions.ToList();
+            a.Add(new Question());
+            var b = a[0].Answers.ToList();
+            for(int i = 0; i < 4; i++)
+            {
+
+                a.Add(new Question() { Text = "Some question", ImageURL = "",});
+                b.Add(new Answer() { Text = "1" });
+                b.Add(new Answer() { Text = "1" });
+                b.Add(new Answer() { Text = "1" });
+
+                //test.Questions.ToList().Add(new Question() { Text = "Some question", ImageURL = "", });
+                //(test.Questions.ToList()[0]).Answers.ToList().Add(new Answer() { Text = "1" });
+                //(test.Questions.ToList()[0]).Answers.ToList().Add(new Answer() { Text = "1" });
+                //(test.Questions.ToList()[0]).Answers.ToList().Add(new Answer() { Text = "1" });
+            }
+
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Children.Add(new Image());
+                stackPanel.Children.Add(new TextBlock());
+                ((Image)stackPanel.Children[0]).Source = (ImageSource)new ImageSourceConverter().ConvertFrom(a[0].ImageURL);
+            ((TextBlock)stackPanel.Children[1]).Text = a[0].Text;
+                for (int i = 0; i < b.Count; i++)
+                {
+                    stackPanel.Children.Add(new CheckBox() { Content = b[i].Text });
+                }
+                if (a[0].ImageURL == "") stackPanel.Children[0].Visibility = Visibility.Hidden;
+                StackPanel_Test.Children.Add(stackPanel);
+
+
+
+            ////////////
+
+
+
+
+
+            // код надо протестить должен работать
+            //foreach (Question question in test.Questions)
+            //{
+            //    StackPanel stackPanel = new StackPanel();
+            //    stackPanel.Children.Add(new Image());
+            //    stackPanel.Children.Add(new TextBlock());
+            //    ((Image)stackPanel.Children[0]).Source = (ImageSource)new ImageSourceConverter().ConvertFrom(question.ImageURL);
+            //    ((TextBlock)stackPanel.Children[1]).Text = question.Text;
+            //    for (int i = 0; i < question.Answers.Count(); i++)
+            //    {
+            //        stackPanel.Children.Add(new CheckBox() { Content = question.Answers.ToList()[i].Text });
+            //    }
+            //   if (question.ImageURL == "") stackPanel.Children[0].Visibility = Visibility.Hidden;
+            //    StackPanel_Test.Children.Add(stackPanel);
+            //}
         }
     }
 }
